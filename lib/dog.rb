9c34d40dba757b34808d4825727ae238fa627af6
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-29 04:57:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = snoop
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+snoop = Dog.new
+snoop.name = "Snoop"
+ 
+puts snoop.name
